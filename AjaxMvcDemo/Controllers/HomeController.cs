a2df77f4ddff5eb76a71ed4b110ea7a1334e85c8
Eduardo Flores <eduflormir@gmail.com>
2015-12-10 14:11:46 +0100

    Demo Ajax JQuery Mvc
    En esta demostración el usuario podra realizar una Reserva introduciendo
    sus datos y al pulsar el botón “Guardar” se enviará información al
    servidor, este la procesará y en función de los datos que recibe el
    Controlador se mostrará una vista parcial u otra, entonces la respuesta
    se devolverá al cliente.

---
+++ AjaxMvcDemo/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AjaxMvcDemo.Models;
+
+namespace AjaxMvcDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ObtenerReserva(String nombre, int numPersonas, String fecha)
+        {
+            
+
+            if (!string.IsNullOrWhiteSpace(nombre))
+            {
+                var numReserva = Convert.ToDateTime(fecha).Day + DateTime.Now.Second;
+                ReservaViewModel reserva = new ReservaViewModel();
+                reserva.Nombre = nombre;
+                reserva.NumPersonas = numPersonas;
+                reserva.Fecha = fecha;
+                reserva.NumReserva = numReserva;
+                reserva.Estado = "Confirmada";
+                
+                return PartialView("Reserva", reserva);
+            }
+            
+            return PartialView("ReservaAnonima");
+
+            //String cadena = $"Nombre: {nombre} Numero: {numPersonas} Fecha: {fecha}";
+            //return Content(cadena);
+        }
+    }
+}
---
+++ AjaxMvcDemo/Models/ReservaViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Security.AccessControl;
+
+namespace AjaxMvcDemo.Models
+{
+    public class ReservaViewModel
+    {
+        public String Nombre { get; set; }
+        public int NumPersonas { get; set; }
+        public String Fecha { get; set; }
+        public int NumReserva { get; set; }
+        public String Estado { get; set; }
+    }
+}
